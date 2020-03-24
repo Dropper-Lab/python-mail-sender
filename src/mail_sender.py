b3be0aa3e532ab5b36005cdbeafcece19deb5db9
nulLeeKH <i_am@nulleekh.com>
2020-03-24 15:18:11 +0900

    [ADD] Create src/mail_sender.py

---
+++ src/mail_sender.py
@@ -0,0 +1,55 @@
+"""
+MIT License
+
+Copyright (c) 2020 Dropper Lab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+import time
+
+import smtplib
+from email.mime.text import MIMEText
+
+import mail_property
+
+logging.Formatter.converter = time.gmtime
+logger = logging.getLogger(__name__)
+fileHandler = RotatingFileHandler('./log/mail_sender.log', maxBytes=1024 * 1024 * 1024 * 9, backupCount=9)
+fileHandler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)s] >> %(message)s'))
+logger.addHandler(fileHandler)
+logger.setLevel(logging.INFO)
+logger.info('every package loaded and start logging')
+
+
+def send_mail(origin=mail_property.mail_address, target=mail_property.mail_address, subject='', message=''):
+    smtp = smtplib.SMTP_SSL(mail_property.address, mail_property.port)
+    smtp.ehlo()
+    smtp.login(mail_property.username, mail_property.password)
+
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = origin
+    msg['To'] = target
+    smtp.sendmail(origin, target, msg.as_string())
+
+    smtp.quit()
